2289e88cac0ce68b2924b7e78c6925350ff860c5
Luka Matijasevic <lukamatijasevic@Dragans-MacBook-Pro.local>
2019-06-27 12:25:31 +0200

    Second Commit

--- gpsApp/App.js
+++ gpsApp/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import ComponentGps from './Component-gps';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,7 +22,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <ComponentGps />
+        <Text style={styles.welcome}>Hello to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
