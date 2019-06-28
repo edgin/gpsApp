7d4a8317ec90b13a161d3a27a23290f1c5ada357
Luka Matijasevic <lukamatijasevic@Dragans-MacBook-Pro.local>
2019-06-28 12:23:46 +0200

    Added button

--- gpsApp/Component-gps.js
+++ gpsApp/Component-gps.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native';
 
 class ComponentGps extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      latitude: null,
-      longitude: null,
+      latitude: 0,
+      longitude: 0,
       error: null,
     };
   }
 
-  componentDidMount() {
+  onPressCalculateGps(){
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
+       (position) => {
+         this.setState({
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude,
+           error: null,
+         });
+       },
+       (error) => this.setState({ error: error.message }),
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 0 },
+     );
   }
+
+  onPressStoreIn(){
+
+  }
+
 
   render() {
     return (
       <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={()=> this.onPressCalculateGps()}
+        title="Calcualate position"
+        color="#841584"
+      />
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
         {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+        <Button
+          onPress={()=>this.onPressStoreIn()}
+          title="Verify"
+          color="#841584"
+        />
       </View>
     );
   }
